826df777424c8bed3fda2207ebc73c895f0f46e1
Caren Chang <carenc@google.com>
2021-09-27 10:55:50 -0700

    Add empty edit task activity

---
+++ app/src/main/java/com/caren/androidbasicsproject3/EditTaskActivity.kt
@@ -0,0 +1,11 @@
+package com.caren.androidbasicsproject3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_task)
+    }
+}
