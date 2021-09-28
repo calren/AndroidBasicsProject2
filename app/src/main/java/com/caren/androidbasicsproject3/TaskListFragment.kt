cd83416eb376fa3ecf46ff6a83dc35364aaa4c68
Caren Chang <carenc@google.com>
2021-09-27 18:03:11 -0700

    Delete unused code in TaskListFragment

--- app/src/main/java/com/caren/androidbasicsproject3/TaskListFragment.kt
+++ app/src/main/java/com/caren/androidbasicsproject3/TaskListFragment.kt
@@ -22,9 +22,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TaskListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     val tasks = mutableListOf<String>()
     var adapter: TaskItemAdapter? = null
@@ -33,10 +30,6 @@ class TaskListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -74,25 +67,5 @@ class TaskListFragment : Fragment() {
             tasks.add(newTask)
             adapter?.notifyItemChanged(tasks.size - 1)
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
